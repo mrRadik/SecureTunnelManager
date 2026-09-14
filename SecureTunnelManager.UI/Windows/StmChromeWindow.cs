f8132c24505dbb3d3bd936c3c7825e60ad8056d2
Файсханов Радик <fayskhanov_rr@astalnalog.ru>
2026-09-14 09:49:35 +0300

    v2.0.1

--- SecureTunnelManager.UI/Windows/StmChromeWindow.cs
+++ SecureTunnelManager.UI/Windows/StmChromeWindow.cs
@@ -1,10 +1,26 @@
 using System.Windows;
+using System.Windows.Controls;
 using SecureTunnelManager.UI.Helpers;
 
 namespace SecureTunnelManager.UI.Windows;
 
 public class StmChromeWindow : Window
 {
+    public static readonly DependencyProperty ShowFrameBorderProperty =
+        DependencyProperty.Register(
+            nameof(ShowFrameBorder),
+            typeof(bool),
+            typeof(StmChromeWindow),
+            new PropertyMetadata(true));
+
+    private bool _frameBorderApplied;
+
+    public bool ShowFrameBorder
+    {
+        get => (bool)GetValue(ShowFrameBorderProperty);
+        set => SetValue(ShowFrameBorderProperty, value);
+    }
+
     public StmChromeWindow()
     {
         WindowStyle = WindowStyle.None;
@@ -34,8 +50,29 @@ public class StmChromeWindow : Window
         NativeWindowHelper.ApplyBorderlessChrome(this);
     }
 
-    private void OnLoaded(object? sender, RoutedEventArgs e) =>
+    private void OnLoaded(object? sender, RoutedEventArgs e)
+    {
+        ApplyFrameBorderIfNeeded();
         NativeWindowHelper.RefreshBorderlessChrome(this);
+    }
+
+    private void ApplyFrameBorderIfNeeded()
+    {
+        if (!ShowFrameBorder || _frameBorderApplied || Content is not UIElement content)
+            return;
+
+        _frameBorderApplied = true;
+        Content = null;
+        Content = new Border
+        {
+            BorderBrush = TryFindResource("StmBorderBrush") as System.Windows.Media.Brush
+                ?? new System.Windows.Media.SolidColorBrush(System.Windows.Media.Color.FromRgb(0x38, 0x3C, 0x48)),
+            BorderThickness = new Thickness(1),
+            SnapsToDevicePixels = true,
+            UseLayoutRounding = true,
+            Child = content
+        };
+    }
 
     private void OnContentRendered(object? sender, EventArgs e) =>
         NativeWindowHelper.RefreshBorderlessChrome(this);
